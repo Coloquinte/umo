17f94c3fe0af77bbbc87c13d0e63536f68f5071b
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2020-06-21 15:18:33 +0200

    Get rid of unneeded packages for Python compilation on Manylinux

--- apis/python/build.sh
+++ apis/python/build.sh
@@ -7,7 +7,7 @@ if [ -z ${PYBIN+"test"} ]
     exit
 fi
 
-$PYBIN/pip install cmake cython twine wheel
+$PYBIN/pip install cmake cython
 mkdir -p cpp_build
 cd cpp_build
 $PYBIN/cmake ../../../ -DCMAKE_BUILD_TYPE=Release
